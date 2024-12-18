96e1f9d0da9f4bbde2a7d2918da1857ca0268400
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2024-12-18 14:29:51 -0600

    Refactor User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
-  validates :username, presence: true, unique: true
-  validates :email, presence: true, unique: true
+  validates :username, presence: true, length: { in: 8..16 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
