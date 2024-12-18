852f593a387207f93fd5ccfd48ffcbb5024bf613
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2024-12-18 15:33:45 -0600

    Add validations for the Post and Comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :content, presence: true, length: { minimum: 1 }
+  validates :user, presence: true
+  validates :post, presence: true
 end
